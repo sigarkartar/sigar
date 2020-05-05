f87ea94ab6efd83b9c7a4d42fd471d9a74b8a5b7
sigarkartar <sigarkartar@gmail.com>
2020-05-05 12:57:43 +0700

    back

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -15,7 +15,7 @@ class Auth extends CI_Controller
         $this->form_validation->set_rules('username', 'Username', 'trim|required');
         $this->form_validation->set_rules('password', 'Password', 'trim|required');
         if ($this->form_validation->run() == false) {
-            $data['title'] = 'Login | SIGAR';
+            $data['title'] = 'Login | PLN RUNGKUT SURABAYA';
             $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/login');
             $this->load->view('templates/auth_footer');
